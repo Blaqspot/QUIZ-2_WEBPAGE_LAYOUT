431f5c17971126792226f3746cf53cb38ab5fe31
David Acquah <scherryprince@gmail.com>
2023-04-18 11:12:06 +0000

    Quiz in webpage layout

---
+++ script.js
@@ -0,0 +1,17 @@
+
+ function show_dropdown1(){
+    document.getElementById("dropdowncat1").style.display="block";
+    }
+
+function hide_dropdown1(){
+    document.getElementById("dropdowncat1").style.display="none"
+    }
+
+function show_dropdown2(){
+    document.getElementById("dropdowncat2").style.display="block";
+}
+
+function hide_dropdown2(){
+    document.getElementById("dropdowncat2").style.display="none";
+}
+
